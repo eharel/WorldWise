cc72d9dc85d404b12fd0cf66880fdb65d4901429
Eli Harel <eliharel3@gmail.com>
2024-07-06 15:10:36 -0600

    Add partial city-CRUD
    Create city and delete city functionality, no update.

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -9,31 +9,72 @@ function CitiesProvider({ children }) {
   const [isLoading, setIsLoading] = useState(false);
   const [currentCity, setCurrentCity] = useState({});
 
-  useEffect(function () {
-    async function fetchCities() {
-      try {
-        setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json();
-        setCities(data);
-      } catch {
-        alert("There was an error loading data");
-      } finally {
-        setIsLoading(false);
-      }
+  async function fetchCities() {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities`);
+      const data = await res.json();
+      setCities(data);
+    } catch {
+      alert("There was an error loading data");
+    } finally {
+      setIsLoading(false);
     }
+  }
 
+  useEffect(function () {
     fetchCities();
   }, []);
 
   async function getCity(id) {
     try {
       setIsLoading(true);
-      const res = await fetch(`${BASE_URL}/cities?id=${id}`);
+      // const res = await fetch(`${BASE_URL}/cities?id=${id}`);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
       const data = await res.json();
-      setCurrentCity(data[0]);
+      // setCurrentCity(data[0]);
+      setCurrentCity(data);
     } catch {
       alert("There was an error fetching the city...");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function createCity(newCity) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities`, {
+        method: "POST",
+        body: JSON.stringify(newCity),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+
+      // fetchCities();
+      setCities((cities) => [...cities, data]);
+      setCurrentCity(data);
+    } catch {
+      alert("There was an error creating the city...");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true);
+      await fetch(`${BASE_URL}/cities/${id}`, {
+        method: "DELETE",
+      });
+      setCities((cities) => cities.filter((city) => city.id !== id));
+      if (currentCity.id === id) {
+        setCurrentCity(cities[0] || null);
+      }
+    } catch (error) {
+      alert("There was an error deleting the city...");
     } finally {
       setIsLoading(false);
     }
@@ -45,7 +86,10 @@ function CitiesProvider({ children }) {
         cities,
         isLoading,
         currentCity,
+        setCurrentCity,
         getCity,
+        createCity,
+        deleteCity,
       }}
     >
       {children}
